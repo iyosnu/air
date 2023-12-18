22a18c83b3f29220c2c2aead88e89aa400b3a60b
iyos <chaiyos.n@takeitsolution.com>
2023-12-18 21:15:24 +0700

    แก้ไข service uuid

--- liff.js
+++ liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = 'a91a0cb4-3652-406f-9de9-e1816a0eca3e'; // LED, Button
+const USER_SERVICE_UUID         = '48dc8e3e-4921-4816-82c1-74fbe03623a6'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
